9da28654a26c8e51335c75b95412cf6caa8eb799
Matteo Muscella <matteo.muscella@mail.com>
2018-10-20 16:57:38 +0200

    Refactors Game, GameGrid and Player classes, adds a GameConfig, adds a GameManager class.

--- Sources/TicTacToe/TicTacToe.swift
+++ Sources/TicTacToe/TicTacToe.swift
@@ -1,3 +1,37 @@
+class GameConfig {
+  let width: Int
+  let height: Int
+  let nOfPlayers: Int
+  let marks: [String]
+  let emptyMark: String
+
+  init(width w: Int, height h: Int, players p: [String], empty e: String) {
+    self.width = w
+    self.height = h
+    self.nOfPlayers = p.count
+    self.marks = p
+    self.emptyMark = e
+  }
+}
+
+class KataConfig: GameConfig {
+  init() {
+    super.init(width: 3, height: 3, players: ["X", "O"], empty: " ")
+  }
+}
+
+extension GameConfig {
+
+  func createGameGrid() -> GameGrid {
+    return GameGrid(width: self.width, height: self.height, emptyMark: self.emptyMark)
+  }
+
+  func createPlayers() -> [Player] {
+    return self.marks.map { Player(mark: $0) }
+  }
+
+}
+
 struct Player {
   let mark: String
 
@@ -9,23 +43,26 @@ struct Player {
 struct GameGrid {
   let width: Int
   let height: Int
+  let emptyMark: String
 
   let grid: [String]
 
-  init(width w: Int, height h: Int) {
+  init(width w: Int, height h: Int, emptyMark e: String) {
     self.width = w
     self.height = h
-    self.grid = Array(repeating: " ", count: w * h)
+    self.emptyMark = e
+    self.grid = Array(repeating: e, count: w * h)
   }
 
-  init(width w: Int, height h: Int, grid g: [String]) {
+  init(width w: Int, height h: Int, emptyMark e: String, grid g: [String]) {
     self.width = w
     self.height = h
     self.grid = g
+    self.emptyMark = e
   }
 
   func isFull() -> Bool {
-    return self.grid.filter { $0 == " " }.count == 0
+    return self.grid.filter { $0 == self.emptyMark }.count == 0
   }
 
   func count() -> Int {
@@ -42,13 +79,23 @@ struct Game {
     self.players = [Player]()
   }
 
-  init(players ps: [Player]) {
-    self.fields = GameGrid(width: 3, height: 3)
+  init(grid g: GameGrid, players ps: [Player]) {
+    self.fields = g
     self.players = ps
   }
 
   func isOver() -> Bool {
     return self.fields.isFull()
+  }
+}
+
+struct GameManager {
+  var game: Game
+  var currentPlayer: Player?
+
+  init(config cfg: GameConfig) {
+    self.game = Game(grid: cfg.createGameGrid(), players: cfg.createPlayers())
+    self.currentPlayer = nil
   }
 }
 
--- Tests/TicTacToeTests/TicTacToeTests.swift
+++ Tests/TicTacToeTests/TicTacToeTests.swift
@@ -10,18 +10,18 @@ final class TicTacToeTests: QuickSpec {
     describe("a game") {
 
       it("has 9 fields") {
-        let game = Game(grid: GameGrid(width: 3, height: 3))
+        let game = Game(grid: GameGrid(width: 3, height: 3, emptyMark: " "))
         expect(game.fields.grid.count).to(equal(9))
       }
 
       it("has a width X height of 3x3") {
-        let game = Game(grid: GameGrid(width: 3, height: 3))
+        let game = Game(grid: GameGrid(width: 3, height: 3, emptyMark: " "))
         expect(game.fields.width).to(equal(3))
         expect(game.fields.height).to(equal(3))
       }
 
       it("is over when all fields are taken") {
-        let game = Game(grid: GameGrid(width: 3, height: 3, grid: [
+        let game = Game(grid: GameGrid(width: 3, height: 3, emptyMark: " ", grid: [
           "X", "O", "O",
           "O", "X", "O",
           "X", "O", "X"]))
@@ -30,11 +30,31 @@ final class TicTacToeTests: QuickSpec {
       }
 
       it("there are two players in the game (X and O)") {
-        let game = Game(players: [Player(mark: "X"), Player(mark: "O")])
+        let game = Game(grid: GameGrid(width: 3, height: 3, emptyMark: " "), players: [Player(mark: "X"), Player(mark: "O")])
 
         expect(game.players.count).to(equal(2))
         expect(game.players[0].mark).to(equal("X"))
         expect(game.players[1].mark).to(equal("O"))
+      }
+
+    }
+
+    describe("KataConfig") {
+
+      it("has width and height equal to 3 and has emptyMark equal to ' '") {
+        let kataConfig = KataConfig()
+
+        expect(kataConfig.width).to(equal(3))
+        expect(kataConfig.height).to(equal(3))
+        expect(kataConfig.emptyMark).to(equal(" "))
+      }
+
+      it("has 2 players marks, X and O") {
+        let kataConfig = KataConfig()
+
+        expect(kataConfig.nOfPlayers).to(equal(2))
+        expect(kataConfig.marks[0]).to(equal("X"))
+        expect(kataConfig.marks[1]).to(equal("O"))
       }
 
     }
