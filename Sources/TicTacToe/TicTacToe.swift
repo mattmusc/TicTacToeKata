f9f5138e1ef955a2459dc3b9e6d55be3c90d1804
Matteo Muscella <matteo.muscella@mail.com>
2018-10-20 12:17:24 +0200

    Adds a struct to represent a Player and adds a list of Players to the Game.

--- Sources/TicTacToe/TicTacToe.swift
+++ Sources/TicTacToe/TicTacToe.swift
@@ -1,22 +1,54 @@
-struct Game {
-    let width: Int
-    let height: Int
+struct Player {
+  let mark: String
 
-    let grid: [String]
-
-    init(width w: Int, height h: Int) {
-      self.width = w
-      self.height = h
-      self.grid = Array(repeating: " ", count: w * h)
-    }
-
-    init(width w: Int, height h: Int, grid g: [String]) {
-      self.width = w
-      self.height = h
-      self.grid = g
-    }
-
-    func isOver() -> Bool {
-      return self.grid.filter { $0 == " " }.count == 0
-    }
+  init(mark m: String) {
+    self.mark = m
+  }
 }
+
+struct GameGrid {
+  let width: Int
+  let height: Int
+
+  let grid: [String]
+
+  init(width w: Int, height h: Int) {
+    self.width = w
+    self.height = h
+    self.grid = Array(repeating: " ", count: w * h)
+  }
+
+  init(width w: Int, height h: Int, grid g: [String]) {
+    self.width = w
+    self.height = h
+    self.grid = g
+  }
+
+  func isFull() -> Bool {
+    return self.grid.filter { $0 == " " }.count == 0
+  }
+
+  func count() -> Int {
+    return grid.count
+  }
+}
+
+struct Game {
+  let fields: GameGrid
+  let players: [Player]
+
+  init(grid g: GameGrid) {
+    self.fields = g
+    self.players = [Player]()
+  }
+
+  init(players ps: [Player]) {
+    self.fields = GameGrid(width: 3, height: 3)
+    self.players = ps
+  }
+
+  func isOver() -> Bool {
+    return self.fields.isFull()
+  }
+}
+
--- Tests/TicTacToeTests/TicTacToeTests.swift
+++ Tests/TicTacToeTests/TicTacToeTests.swift
@@ -10,24 +10,31 @@ final class TicTacToeTests: QuickSpec {
     describe("a game") {
 
       it("has 9 fields") {
-        let game = Game(width: 3, height: 3)
-        expect(game.grid.count).to(equal(9))
+        let game = Game(grid: GameGrid(width: 3, height: 3))
+        expect(game.fields.grid.count).to(equal(9))
       }
 
       it("has a width X height of 3x3") {
-        let game = Game(width: 3, height: 3)
-        expect(game.width).to(equal(3))
-        expect(game.height).to(equal(3))
+        let game = Game(grid: GameGrid(width: 3, height: 3))
+        expect(game.fields.width).to(equal(3))
+        expect(game.fields.height).to(equal(3))
       }
 
       it("is over when all fields are taken") {
-        let game = Game(width: 3, height: 3, grid: [
+        let game = Game(grid: GameGrid(width: 3, height: 3, grid: [
           "X", "O", "O",
           "O", "X", "O",
-          "X", "O", "X"])
+          "X", "O", "X"]))
 
         expect(game.isOver()).to(equal(true))
       }
+
+      it("there are two players in the game (X and O)") {
+        let game = Game(players: [Player(mark: "X"), Player(mark: "O")])
+
+        expect(game.players.count).to(equal(2))
+      }
+
     }
 
   }
